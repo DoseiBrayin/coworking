6d0173cb78a5c7b4dee054c39bb12e94516a53e6
DoseiBrayin <ardilabrayan68@gmail.com>
2023-10-18 00:49:55 -0500

    practica de fetch y llamadas a API

---
+++ fetch2/fetch.js
@@ -0,0 +1,34 @@
+const container = document.querySelector("#container");
+const numeralComments = document.querySelector("#numeral-comments");
+const img = document.createElement("img");
+img.src = "./public/images/loading-gif.webp";
+img.id = "loading-gif";
+
+const btnFetch = () => {
+    container.appendChild(img);
+    setTimeout(traerDatos, 1000);
+}
+
+const traerDatos = () => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+        .then(data => data.json())
+        .then(data => mostrarComments(data))
+        .catch((error) => {
+            alert("Error al traer los datos");
+            console.error(error);
+        })
+        .finally(()=> container.removeChild(img))
+}
+
+const mostrarComments = (data) => {
+    const comments = data.map(comment => {
+        const li = document.createElement("li");
+        li.textContent = comment.body;
+        return li;
+    })
+    comments.map(element => numeralComments.appendChild(element))
+}
+
+
+
+
